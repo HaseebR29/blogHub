3c778db27529953a5e3348178f061b29170edaad
Haseebur Rehman <140433525+HaseebR29@users.noreply.github.com>
2024-05-16 02:06:52 +0530

    user.js

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-var image = "./public/images/default"
+var image = "./public/images/default.avif"
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
